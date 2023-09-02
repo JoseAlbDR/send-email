1f3041a04e8f55442bea2156cf81dc53c120c94f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-02 12:10:34 +0200

    feat: send email with nodemailer and ethereal

--- src/controllers/sendEmail.ts
+++ src/controllers/sendEmail.ts
@@ -1,4 +1,21 @@
 import { Request, Response } from "express";
+import nodemailer from "nodemailer";
 export const sendEmail = async (_req: Request, res: Response) => {
-  res.send("send email route");
+  // const testAccount = await nodemailer.createTestAccount();
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "mario.feil@ethereal.email",
+      pass: "YRWNdQgJk3Jdm3C6mv",
+    },
+  });
+  const info = await transporter.sendMail({
+    from: "J.Alberto Delgado <jaderodev@gmail.com>",
+    to: "bar@example.com",
+    subject: "Hello",
+    html: "<h2>Sending Emails with Node.js</h2>",
+  });
+
+  res.json(info);
 };
